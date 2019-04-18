5bb3b83827dafb03d4c0152e7240f2784f95659f
Michael Kohler <me@michaelkohler.info>
2019-04-18 02:49:04 +0200

    Create dummy impl for creating issues

--- src/github.rs
+++ src/github.rs
@@ -3,6 +3,7 @@ use crate::bugzilla;
 
 use reqwest::header::AUTHORIZATION;
 use std::env;
+use std::fmt;
 
 const GITHUB_OWNER_ENV_NAME: &str = "GITHUB_OWNER";
 const GITHUB_REPO_ENV_NAME: &str = "GITHUB_REPO";
@@ -12,6 +13,18 @@ const URL: &str = "https://api.github.com/user/issues";
 pub struct Issue {
     pub html_url: String,
     pub title: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TaskIssue {
+    title: String,
+    body: String,
+}
+
+impl fmt::Display for TaskIssue {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}: {}", self.title, self.body)
+    }
 }
 
 pub fn get_issues(auth_value: String) -> Result<Vec<Issue>, Box<std::error::Error>> {
@@ -29,14 +42,28 @@ pub fn create_issue_from_bugzilla(bug: &bugzilla::Bug) {
     let (github_owner, github_repo) = get_env();
 
     info!("Creating issue in {}/{} for {}", github_owner, github_repo, bug.id);
-    error!("Missing implementation..");
+    let issue = TaskIssue {
+        title: format!("{}", bug.summary), // TODO: fix ownership for real...
+        body: format!("https://bugzilla.mozilla.org/show_bug.cgi?id={}", bug.id),
+    };
+    create_issue(issue);
 }
 
 pub fn create_issue_from_github(issue: &Issue) {
     let (github_owner, github_repo) = get_env();
 
     info!("Creating issue in {}/{} for {}", github_owner, github_repo, issue.html_url);
-    error!("Missing implementation..");
+    let issue = TaskIssue {
+        title: format!("{}", issue.title), // TODO: fix ownership for real...
+        body: format!("https://bugzilla.mozilla.org/show_bug.cgi?id={}", issue.html_url),
+    };
+    create_issue(issue);
+}
+
+fn create_issue(issue: TaskIssue) {
+    debug!("Creating issue: {}", issue);
+    error!("Missing implementation");
+    panic!("MISSING_IMPLEMENTATION");
 }
 
 fn get_env() -> (String, String) {
